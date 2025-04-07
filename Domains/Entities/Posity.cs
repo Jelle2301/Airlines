a46bf9a4f30ef2144f8d047474ebe8877f7189e2
Quinten Bernard <r0980497@student.vives.be>
2025-04-07 13:42:24 +0200

    database

---
+++ Domains/Entities/Posity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domains.Entities;
+
+public partial class Posity
+{
+    public int PositieId { get; set; }
+
+    public string SoortPositie { get; set; } = null!;
+}
---
+++ Domains/Entities/Team.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domains.Entities;
+
+public partial class Team
+{
+    public int TeamId { get; set; }
+
+    public string TeamNaam { get; set; } = null!;
+}
