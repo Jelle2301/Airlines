87fba4e2fe7736a99cdc105034d34c2a90e31ab4
Quinten Bernard <r0980497@student.vives.be>
2025-04-07 16:31:36 +0200

    Luchthavenscontroller

--- Airlines/Controllers/API/LuchthavensController.cs
+++ Airlines/Controllers/API/LuchthavensController.cs
@@ -1,10 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Airlines.ViewModels;
+using AutoMapper;
+using Domains.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
 
 namespace Airlines.Controllers.API
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class LuchthavensController : Controller
     {
-        public IActionResult Index()
+        private IService<Plaats> _plaatsService;
+        private readonly IMapper _mapper;
+        public LuchthavensController(IService<Plaats> plaatsService, IMapper mapper)
+        {
+            _plaatsService = plaatsService;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<ActionResult<PlaatsVM>> Get()
         {
             return View();
         }
