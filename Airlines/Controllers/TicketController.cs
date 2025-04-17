dec04f2ff11572e5c61a6d2ce6d92e0cd98a2303
Quinten Bernard <r0980497@student.vives.be>
2025-04-17 22:05:56 +0200

    Vluchten kan je nu kiezen, gekozen vlucht wordt doorgestuurd naar andere controller

---
+++ Airlines/Controllers/TicketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Airlines.Controllers
+{
+    public class TicketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
