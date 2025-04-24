07978ff73744dc3156e227571c1516c92fbfd4ef
Quinten Bernard <r0980497@student.vives.be>
2025-04-24 21:51:53 +0200

    Enkele dingen getest

---
+++ Airlines/ViewModels/TicketGekozenVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Airlines.ViewModels
+{
+    public class TicketGekozenVM
+    {
+        [Required]
+        public string Voornaam { get; set; } = null!;
+        [Required]
+        public string Achternaam { get; set; } = null!;
+        public double TotaalPrijs { get; set; }
+
+    }
+}
