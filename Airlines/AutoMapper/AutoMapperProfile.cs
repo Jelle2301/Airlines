aec5b7ca5c62a1967b8686a54154268a7867ad36
Quinten Bernard <r0980497@student.vives.be>
2025-05-06 10:15:42 +0200

    Aanpassing automapper

--- Airlines/AutoMapper/AutoMapperProfile.cs
+++ Airlines/AutoMapper/AutoMapperProfile.cs
@@ -62,39 +62,39 @@ namespace Airlines.AutoMapper
 
             //van VM naar Entity
             CreateMap<PlaatsVM, Plaats>();
-
             CreateMap<VluchtVM, Vlucht>()
                 .ForMember(dest => dest.Prijs,
                     opts => opts.MapFrom(
                         src => src.BeginPrijs))
-                .ForMember(dest => dest.Vliegtuig.VliegtuigNaam,
+                .ForPath(dest => dest.Vliegtuig.VliegtuigNaam,
                     opts => opts.MapFrom(
                         src => src.VliegtuigNaam))
-                .ForMember(dest => dest.Vliegtuig.AantalTotalePlaatsen,
+                .ForPath(dest => dest.Vliegtuig.AantalTotalePlaatsen,
                     opts => opts.MapFrom(
                         src => src.AantalTotalePlaatsen))
-                .ForMember(dest => dest.Vliegtuig.AantalGewonePlaatsen,
+                .ForPath(dest => dest.Vliegtuig.AantalGewonePlaatsen,
                     opts => opts.MapFrom(
                         src => src.AantalGewonePlaatsen))
-                .ForMember(dest => dest.Vliegtuig.AantalBusinessPlaatsen,
+                .ForPath(dest => dest.Vliegtuig.AantalBusinessPlaatsen,
                     opts => opts.MapFrom(
                         src => src.AantalBusinessPlaatsen))
-                .ForMember(dest => dest.Vliegtuig.AantalEconomyPlaatsen,
+                .ForPath(dest => dest.Vliegtuig.AantalEconomyPlaatsen,
                     opts => opts.MapFrom(
                         src => src.AantalEconomyPlaatsen))
-                .ForMember(dest => dest.Vertrekplaats.Plaats.Naam,
+                .ForPath(dest => dest.Vertrekplaats.Plaats.Naam,
                     opts => opts.MapFrom(
                         src => src.VertrekNaam))
                 /*.ForMember(dest => dest.OverstapNaam,
                     opts => opts.MapFrom(
                         src => src.Overstap.Plaats.Naam))*/
-                .ForMember(dest => dest.Bestemming.Plaats.Naam,
+                .ForPath(dest => dest.Bestemming.Plaats.Naam,
                     opts => opts.MapFrom(
                         src => src.BestemmingNaam))
                 .ForMember(dest => dest.IsOverstap,
                     opts => opts.MapFrom(
                         src => src.IsOverstap))
                 ;
+
 
             CreateMap<MaaltijdVM, Maaltijd>()
                 .ForMember(dest => dest.Prijs,
