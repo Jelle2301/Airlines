95b9059a89acaf955a19717b66e9f6aac01b0b77
Jelle2301 <146099496+Jelle2301@users.noreply.github.com>
2025-05-07 15:24:40 +0200

    Api call voor vluchten tussen 2 luchthavens gemaakt

---
+++ Airlines/Controllers/API/VluchtController.cs
@@ -0,0 +1,32 @@
+﻿using Airlines.ViewModels;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+using Services.Interfaces;
+
+namespace Airlines.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VluchtController : Controller
+    {
+        private readonly IMapper _mapper;
+        private IVluchtService _vluchtService;
+        public VluchtController(IMapper mapper, IVluchtService vluchtService)
+        {
+            _mapper = mapper;
+            _vluchtService = vluchtService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<VluchtVM>> GetVluchtenTussenPlaatsen(int vertrekPlaatdId, int bestemmingId)
+        {
+           var vluchten = await _vluchtService.GetVluchtenTussenPlaatsen(vertrekPlaatdId, bestemmingId);
+            if (vluchten == null)
+            {
+                return NotFound("No flights found");
+            }
+            var vluchtenVMs = _mapper.Map<IEnumerable<VluchtVM>>(vluchten);
+            return Ok(vluchtenVMs);
+        }
+    }
+}
